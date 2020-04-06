dcde9c789a4e957e36f9480e428a956c51f30a93
oran <oranable@qq.com>
2020-04-06 21:46:08 +0800

    第一次啊

---
+++ after/bbt_spring_recruit_2020/config.py
@@ -0,0 +1,13 @@
+#配置文件
+
+db = {
+    'host' : 'localhost',
+    'user' : 'BBT', 
+    'passwd' : '1693189780',
+    'database' : 'spring_recruit_2020',
+    'charset' : 'utf8mb4'
+}
+
+app = {
+    'SECRET_KEY':'cnoaifh19484alsg&%&'#随机字符串
+}
